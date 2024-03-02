6812d28f0ec211900cbc0fd869a78c32220623d8
kadziewiczjakub <jakubkadziewicz@jkadziewicz.dev>
2024-03-02 22:33:04 +0100

    Added Antlr4 and StringTemplate4

--- Maps/Maps/Program.cs
+++ Maps/Maps/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using Antlr4.StringTemplate;
 Console.WriteLine("Hello, World!");
+
+var st = new Antlr4.StringTemplate.Template("Hello, <name>!");
+st.Add("name", "World from StringTemplate");
+Console.WriteLine(st.Render());
